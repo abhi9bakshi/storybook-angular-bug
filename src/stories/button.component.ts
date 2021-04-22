f1ff85ff2a596e7d3c69724290131810760f8bd6
Abhinav Bakshi <abhi9bakshi@gmail.com>
2021-04-22 16:37:44 +0530

    Minimal reproduction of error

--- src/stories/button.component.ts
+++ src/stories/button.component.ts
@@ -3,13 +3,14 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'storybook-button',
   template: ` <button
-    type="button"
-    (click)="onClick.emit($event)"
-    [ngClass]="classes"
-    [ngStyle]="{ 'background-color': backgroundColor }"
-  >
-    {{ label }}
-  </button>`,
+      type="button"
+      (click)="toggleLabel()"
+      [ngClass]="classes"
+      [ngStyle]="{ 'background-color': backgroundColor }"
+    >
+      {{ label }}
+    </button>
+    <p *ngIf="!hideLabel">Test label</p>`,
   styleUrls: ['./button.css'],
 })
 export default class ButtonComponent {
@@ -24,6 +25,12 @@ export default class ButtonComponent {
    */
   @Input()
   backgroundColor?: string;
+
+  /**
+   * Should the label be hidden
+   */
+  @Input()
+  hideLabel?: boolean;
 
   /**
    * How large should the button be?
@@ -46,8 +53,14 @@ export default class ButtonComponent {
   onClick = new EventEmitter<Event>();
 
   public get classes(): string[] {
-    const mode = this.primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+    const mode = this.primary
+      ? 'storybook-button--primary'
+      : 'storybook-button--secondary';
 
     return ['storybook-button', `storybook-button--${this.size}`, mode];
+  }
+
+  toggleLabel() {
+    this.hideLabel = !this.hideLabel;
   }
 }
